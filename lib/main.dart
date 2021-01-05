ff378ef83392a4c7c94e5c1e1b2a30a0f0ba2020
iyliayasmin <iyliayasmin86@gmail.com>
2021-01-05 21:23:54 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quiz_application/helper/authenticate.dart';
+import 'package:quiz_application/helper/constants.dart';
+import 'package:quiz_application/views/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isUserLoggedIn = false;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await Constants.getUerLoggedInSharedPreference().then((value) {
+      setState(() {
+        isUserLoggedIn = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: isUserLoggedIn ? Home() : Authenticate(),
+    );
+  }
+}
